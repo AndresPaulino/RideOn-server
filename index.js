07b5359f7cea6875617e3b1bc53710181fb8014b
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-11 02:14:26 -0400

    Setup dependencies and index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.port || 8080;
+const userRoutes = require('./routes/users');
+
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('users', userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
