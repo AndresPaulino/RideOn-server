75d35aa7444e430b11c5ab8e5505bcb34313a57b
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-17 14:23:02 -0400

    Add mock data server - fix bugs

--- migrations/20220511063130_create_user.js
+++ migrations/20220511063130_create_user.js
@@ -3,7 +3,7 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable('users', (table) => {
+  return knex.schema.createTable('userex', (table) => {
     table.increments('id').primary();
     table.string('user_name').notNullable().unique;
     table.string('email').notNullable().unique;
--- routes/rides.js
+++ routes/rides.js
@@ -4,14 +4,39 @@ const knex = require('knex')(require('../knexfile'));
 
 // Get Rides
 router.get('/rides', (req, res) => {
-    knex('rides')
-        .then(rides => {
-            res.json(rides);
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
+  knex('rides')
+    .then((rides) => {
+      res.json(rides);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
+// Post Ride
+router.post('/rides', (req, res) => {
+  const { id, user_name, profile_img, title, address, date, time, description, from,  } = req.body;
+  const newRide = {
+    user_name,
+      profile_img,
+      title,
+      address,
+      date,
+      time,
+      description,
+      from,
+      to,
+      id,
+        
+  };
+  knex('rides')
+    .insert(newRide)
+    .then(() => {
+      res.status(201).send('Ride created successfully');
+    })
+    .catch(() => {
+      res.status(400).send('Failed to create ride');
+    });
+});
 
 module.exports = router;
