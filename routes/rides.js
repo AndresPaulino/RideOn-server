fe711ae6fa16226cc33aa1e1fecce4dfa86a9fe1
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-18 17:49:39 -0400

    Add increment participants functionality

--- routes/rides.js
+++ routes/rides.js
@@ -15,22 +15,35 @@ router.get('/rides', (req, res) => {
 
 // Post Ride
 router.post('/rides', (req, res) => {
-  const { id, user_name, profile_img, ride_title, ride_address, ride_date, ride_time, ride_description, ride_from } = req.body;
+  const { id, user_name, profile_img, ride_title, ride_address, ride_date, ride_time, ride_description, ride_from } =
+    req.body;
   const newRide = {
     user_name,
-      profile_img,
-      ride_title,
+    profile_img,
+    ride_title,
     address1: ride_address,
-      address2: ride_address,
-      ride_date,
-      ride_time,
-      ride_description,
-      ride_from,
-      id,
-        
+    address2: ride_address,
+    ride_date,
+    ride_time,
+    ride_description,
+    ride_from,
   };
   knex('rides')
     .insert(newRide)
+    .then(() => {
+      res.status(201).send('Ride created successfully');
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+// Increment participant count
+router.post('/rides/add-participants', (req, res) => {
+  const { id } = req.body;
+  knex('rides')
+    .where({ id })
+    .increment('ride_participants', 1)
     .then(() => {
       res.status(201).send('Ride created successfully');
     })
