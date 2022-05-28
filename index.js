666942b01133f967e914c5196b1c2e3d36543fc7
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-28 13:32:20 -0400

    Add fileupload package and init /upload path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.port || 8080;
@@ -8,9 +9,27 @@ const ridesRoutes = require('./routes/rides');
 const userRidesRoutes = require('./routes/userRides');
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(fileUpload());
+
+// File upload
+app.post('/upload', authenticator, (req, res) => {
+  const imageFile = req.files['image-field'];
+  const fileName = imageFile.name;
+  const uploadPath = `images/${fileName}`;
+  imageFile.mv(`${__dirname}/public/${uploadPath}`, function (err) {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    return res.send({ path: uploadPath });
+  });
+});
 
 
 // Routes
