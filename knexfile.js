307a67bd123d46bae3f72f78c518516a7cc17f7a
Kykeon <paulinoandresmiguel@gmail.com>
2022-06-17 23:31:46 -0400

    Delete migrations and seeds. Deploy server on Azure, change knexfile config to connect to db

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 module.exports = {
   client: 'mysql',
   connection: {
-    host: 'us-cdbr-east-05.cleardb.net',
-    user: 'bd191d788d84b2',
-    password: '562cc002',
-    database: 'heroku_669096072f06393',
+    host: 'ride-on.mysql.database.azure.com',
+    user: 'apaulino01',
+    password: process.env.SERVER_PWD,
+    database: 'ride-on',
     charset: 'utf8',
   },
 };
